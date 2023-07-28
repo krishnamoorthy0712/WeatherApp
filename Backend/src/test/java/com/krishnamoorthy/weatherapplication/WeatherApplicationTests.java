1cadbb89051df530d826f91b09cc1c7f845613f8
Hari Prasath <68268300+Hari-prasath855@users.noreply.github.com>
2023-07-28 14:39:14 +0530

    Update WeatherApplicationTests.java

--- Backend/src/test/java/com/krishnamoorthy/weatherapplication/WeatherApplicationTests.java
+++ Backend/src/test/java/com/krishnamoorthy/weatherapplication/WeatherApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hariprasatht.weatherapplication;
+package com.krishnamoorthy.weatherapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
